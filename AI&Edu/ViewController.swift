445f0d737add0cde950dbd6217ad1a3c9bbff24f
Oybek <treyktoriya@gmail.com>
2025-03-12 17:52:46 +0500

    Initial commit

---
+++ AI&Edu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AI&Edu
+//
+//  Created by Oybek To’laboyev on 12/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
